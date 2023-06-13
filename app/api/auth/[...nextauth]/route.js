6231fc87740a0af93c84bf41a5d35865f75c0251
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-13 18:33:34 +0530

    Setting up user model

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDB } from "@utils/db";
+
 const handler = NextAuth({
 	providers: [
 		GoogleProvider({
@@ -11,7 +13,10 @@ const handler = NextAuth({
 	async session({ session }) {},
 	async signIn({ profile }) {
 		try {
-		} catch (err) {}
+			await connectToDB();
+		} catch (err) {
+			console.log(err);
+		}
 	},
 });
 
