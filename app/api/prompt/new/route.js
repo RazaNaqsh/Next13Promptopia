61c0b078576f9437a368bde32d6fb7ee0ad3d934
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-14 11:25:35 +0530

    New prompt route api

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,10 +1,19 @@
+import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/db";
-import { NextResponse } from "next/server";
 
 export const POST = async (req, res) => {
 	const { userId, prompt, tag } = await req.json();
 
 	try {
 		await connectToDB();
-	} catch (err) {}
+		const newPrompt = new Prompt({
+			creator: userId,
+			prompt,
+			tag,
+		});
+		await newPrompt.save();
+		return new Response(JSON.stringify(newPrompt), { status: 201 });
+	} catch (err) {
+		return new Response("Failed to create a new prompt", { status: 500 });
+	}
 };
