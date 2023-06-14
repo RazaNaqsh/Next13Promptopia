d7b9abe8d5cc532dea599630ec35525fce590655
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-14 16:09:23 +0530

    Delete endpoint

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -28,6 +28,18 @@ export const PATCH = async (req, { params }) => {
 		await existingPrompt.save();
 		return new Response(JSON.stringify(existingPrompt), { status: 200 });
 	} catch (err) {
-		return new Response("Failed to update prompts", { status: 500 });
+		return new Response("Failed to update prompt", { status: 500 });
+	}
+};
+
+export const DELETE = async (req, { params }) => {
+	try {
+		await connectToDB();
+
+		await Prompt.findByIdAndRemove(params.id);
+
+		return new Response("Prompt deleted succesfully", { status: 200 });
+	} catch (err) {
+		return new Response("Failed to delete prompt", { status: 500 });
 	}
 };
